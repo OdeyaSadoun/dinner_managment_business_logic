1007d211b1983fc8ed11d7dca75a52bd69f7267f
אודיה סעדון <osadoun@g.jct.ac.il>
2024-11-20 18:12:15 +0200

    add implement to interfaces

--- src/api/controllers/person_controller.py
+++ src/api/controllers/person_controller.py
@@ -1,13 +1,14 @@
 from globals.consts.const_strings import ConstStrings
 from globals.consts.zmq_const_strings import ZMQConstStrings
 from globals.enums.response_status import ResponseStatus
+from infrastructures.interfaces.icontroller_managment import IControllerManager
 from infrastructures.interfaces.izmq_client_manager import IZMQClientManager
 from models.data_classes.zmq_response import Response
 from models.data_classes.zmq_request import Request
 from models.data_classes.person import Person
 
 
-class PersonController:
+class PersonController(IControllerManager):
     def __init__(self, data_zmq_client: IZMQClientManager):
         self._data_zmq_client = data_zmq_client
         
--- src/globals/consts/const_strings.py
+++ src/globals/consts/const_strings.py
@@ -2,8 +2,8 @@ class ConstStrings:
     # ? ENV keys
     localhost_env_key = "LOCAL_HOST"
     database_gateway_host_env_key = "DATABASE_GATEWAY_HOST"
-    local_port = "LOCAL_PORT"
-    database_gateway_port = "DATABASE_GATEWAY_PORT"
+    local_port_env_key = "LOCAL_PORT"
+    database_gateway_port_env_key = "DATABASE_GATEWAY_PORT"
     
     username_key = "username"
     password_key = "password"
